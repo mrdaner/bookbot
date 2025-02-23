9142540f33da723eaf3471fcc316fabf7d58b671
mrdaner <lakhnodima@gmail.com>
2025-02-23 17:30:44 +0000

    changed report

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import sys
 from stats import *
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    
+    path = sys.argv[1]
+    
     with open(path) as f:
         file_contents = f.read()
-        print(f"--- Begin report of {path} ---")
-        print(f"{get_num_words(file_contents)} words found in the document")
-
+        print(f"============ BOOKBOT ============")
+        print(f"Analyzing book found at {path}...")
+        print(f"----------- Word Count ----------")
+        print(f"Found {get_num_words(file_contents)} total words")
+        print(f"--------- Character Count -------")
         char_counts = count_characters(file_contents)
         for char_data in char_counts:
-            print(f"The '{char_data["char"]}' character was found {char_data["num"]} times")
+            print(f"{char_data["char"]}: {char_data["num"]}")
 
-        print(f"--- End report ---")
+        print(f"============= END ===============")
+
 main()
